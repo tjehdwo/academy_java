5aed65339cb1b4b9d33985614571619ecf51eeb1
tjehdwo <143372343+tjehdwo@users.noreply.github.com>
2023-08-31 17:14:13 +0900

    java

--- Practice/src/com/kh/practice1/func/VariablePractice4.java
+++ Practice/src/com/kh/practice1/func/VariablePractice4.java
@@ -6,11 +6,19 @@ public class VariablePractice4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-    Scanner sc = new Scanner(Sysyem.in);
+    Scanner sc = new Scanner(System.in);
     
     System.out.println("문자열을 입력하세요. :");
     String s = sc.next();
     
-	}
+    char a = s.charAt(0);
+    System.out.println("index 0번째 자리"+a);
+    
+    char p = s.charAt(1);
+    System.out.println("index 1번째 자리 :"+p);
+	
+    char p2 = s.charAt(2);
+    System.out.println("index 2번째 자리 :"+p2);
+    }
 
 }
