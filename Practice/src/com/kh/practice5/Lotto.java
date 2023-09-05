8312efd053296f9d512887cdb048c1370afe8fea
tjehdwo <143372343+tjehdwo@users.noreply.github.com>
2023-09-05 17:29:07 +0900

    Practice

--- Practice/src/com/kh/practice5/Lotto.java
+++ Practice/src/com/kh/practice5/Lotto.java
@@ -20,7 +20,14 @@ public class Lotto {
 		for(int i = 0;i<lotto.length;i++) {
 			int num = (int)(Math.random()*45)+1;
 			lotto[i]= num;
-		}
+				for(int j =0;j<i;j++) {
+				if(lotto[i] == lotto[j]) {
+					i--;
+					break;
+				}
+				}
+				System.out.print(lotto[i] + " ");
+				}
 	}
 }
 	
