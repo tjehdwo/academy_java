1f963c741405586d37cf910eb6ef572466bb57e6
tjehdwo <143372343+tjehdwo@users.noreply.github.com>
2023-09-06 17:19:55 +0900

    Inheritance

---
+++ chap10_Inheritance/src/com/kh/inheritance/calculator/CalculatorForMod.java
@@ -0,0 +1,7 @@
+package com.kh.inheritance.calculator;
+
+public class CalculatorForMod extends Calculator{
+	public int mod() {
+		return num1 %num2;
+	}
+}
--- chap10_Inheritance/src/com/kh/inheritance/calculator/CalculatorRun.java
+++ chap10_Inheritance/src/com/kh/inheritance/calculator/CalculatorRun.java
@@ -6,7 +6,18 @@ public class CalculatorRun extends Calculator{
 	
 
 	public static void main(String[] args) {
-		Calculator cal = new Calculator();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("숫자 1 :");
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		CalculatorForMod cal = new CalculatorForMod();
+		System.out.println("add = "+ cal.add());
+		System.out.println("mul = "+ cal.multiply());
+		System.out.println("mod = " + cal.mod());
+		
+	}
+}
+		/*Calculator cal = new Calculator();
 		Scanner sc = new Scanner(System.in);
 		System.out.println("첫번째 수를 입력하세요.");
 		int num1 = sc.nextInt();
@@ -14,10 +25,10 @@ public class CalculatorRun extends Calculator{
 		System.out.println("두번째 수를 입력하세요.");
 		int num2 = sc.nextInt();
 		
-		int result1 = cal.add(num1,num2);
-		int result2 = cal.subsruct(num1,num2);
-		int result3 = cal.multiply(num1,num2);
-		int result4 = cal.divide(num1,num2);
+		int result1 = cal.add();
+		int result2 = cal.subsruct();
+		int result3 = cal.multiply();
+		int result4 = cal.divide();
 
 		System.out.println("Add : "+ result1);
 		System.out.println("sub : "+ result2);
