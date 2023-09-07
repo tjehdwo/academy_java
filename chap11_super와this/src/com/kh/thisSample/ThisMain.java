f9d8c404a17deff5331535c380397c90cc2d10bd
tjehdwo <143372343+tjehdwo@users.noreply.github.com>
2023-09-07 11:19:07 +0900

    super,this

---
+++ chap11_super와this/src/com/kh/thisSample/ThisMain.java
@@ -0,0 +1,37 @@
+package com.kh.thisSample;
+/*
+* int x = 10; //정수 x를 10값 할당
+* int y=x;    //참조변수 y x를 가리킨다
+* 
+* x=20
+* y =x;
+* this 인스턴스 자신을 가리키는 참조변수 인스턴스의 주소가 저장
+* this 객체(=인스턴스) 자신을 가리키는 참조변수 객체(=인스턴스)의 주소가 저장
+* this() 현재 클래스의 다른 생성자를 호출하는데 사용
+*/
+class MyClass{
+	int myField;
+	
+	//첫 번째 생성자
+	MyClass(){
+		this(0); //this() 사용해서 다른 생성자 호출
+	}
+	//두 번째 생성자
+	MyClass(int value){
+		this.myField = value;
+	}
+	void printValue() {
+		System.out.println("myField의 값 : "+this.myField);
+	}
+}
+public class ThisMain {
+
+	public static void main(String[] args) {
+		MyClass obj1 = new MyClass();//첫 번째 생성자 호출
+		MyClass obj2 = new MyClass(10);//두 번째 생성자 호출
+		
+		obj1.printValue();
+		obj2.printValue();
+	}
+
+}
