ba241e4e31c93f5f8ef513ca5dfc4c291703bbd0
tjehdwo <143372343+tjehdwo@users.noreply.github.com>
2023-09-07 14:33:41 +0900

    Variable

---
+++ chap01_Variable/src/chap01_Variable/ReferencVariable.java
@@ -0,0 +1,19 @@
+package chap01_Variable;
+
+import java.util.Scanner;
+
+public class ReferencVariable {
+
+	public static void main(String[] args) {
+		int num = 10; //정수형 지역변수
+		int b = num;  //변수 값을 대입한다.
+/*********************************************************/		
+		//객체(=인스턴스)
+		Scanner sc;   //객체 (=인스턴스) 변수
+		
+		String s;
+		
+		final int c;  //상수
+	}
+
+}
