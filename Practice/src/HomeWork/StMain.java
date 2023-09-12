e633321bb6f3bfe43f86d1b1cdc105a71a4a770d
tjehdwo <143372343+tjehdwo@users.noreply.github.com>
2023-09-12 14:41:21 +0900

    practice

---
+++ Practice/src/HomeWork/StMain.java
@@ -0,0 +1,16 @@
+package HomeWork;
+
+public class StMain {
+
+	public static void main(String[] args) {
+		Student3[] student = new Student3[4];
+		
+		
+		student[0] = new Student3("김길동","자바",100);
+		student[1] = new Student3("박길동","디비",100);
+		student[2] = new Student3("이길동","화면",100);
+		student[3] = new Student3("정길동","서버",100);
+		student[4] = new Student3("홍길동","자바",20);
+	}
+
+}
---
+++ Practice/src/HomeWork/Student3.java
@@ -0,0 +1,14 @@
+package HomeWork;
+
+public class Student3 {
+	String name;
+	String subject;
+	int score;
+	
+	public Student3(String name,String subject, int score) {
+		this.name = name;
+		this.subject = subject;
+		this.score = score;
+	}
+
+}
