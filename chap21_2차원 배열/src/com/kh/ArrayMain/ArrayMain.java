1f225e068c5557c1bf953e13ac85536bbd238b31
tjehdwo <143372343+tjehdwo@users.noreply.github.com>
2023-09-12 16:10:44 +0900

    2차원배열

---
+++ chap21_2차원 배열/src/com/kh/ArrayMain/ArrayMain.java
@@ -0,0 +1,10 @@
+package com.kh.ArrayMain;
+
+import com.kh.ArrayEaxm.ArrayEaxm;
+
+public class ArrayMain {
+	public static void main(String[] args) {
+		ArrayEaxm practice = new ArrayEaxm();
+		practice.practice4();
+	}
+}
