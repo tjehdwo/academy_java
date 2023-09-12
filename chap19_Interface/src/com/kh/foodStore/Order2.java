404448f5f7c00a0dbd27b5a3776bebab7c64c6a9
tjehdwo <143372343+tjehdwo@users.noreply.github.com>
2023-09-12 16:10:08 +0900

    interface

--- chap19_Interface/src/com/kh/foodStore/Order2.java
+++ chap19_Interface/src/com/kh/foodStore/Order2.java
@@ -1,5 +1,20 @@
 package com.kh.foodStore;
 
-public class Order2 {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Order2 implements FoodStore{
+	private List<Menu> oder2menus;
+	
+	public Order2() {
+		oder2menus = new ArrayList<>();
+	}
+	
+	@Override
+	public void addMenu(String menu) {
+		Menu menuB = new Menu(menu);
+		oder2menus.add
+	}
+	
 
 }
