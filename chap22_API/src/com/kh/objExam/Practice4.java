75cef05929920a1a14b9b5c491430190979e4c9a
tjehdwo <143372343+tjehdwo@users.noreply.github.com>
2023-09-13 17:13:25 +0900

    api

--- chap22_API/src/com/kh/objExam/Practice4.java
+++ chap22_API/src/com/kh/objExam/Practice4.java
@@ -7,14 +7,14 @@ import java.util.Date;
 public class Practice4 {
 	public static void main(String[] args) {
 		Practice4 p4 = new Practice4();
-		p4.pracitce2();
+		p4.pracitce5();
 	}
 	
 	public void pracitce1() {
 		Date d1 = new Date();
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 		String forDate = sdf.format(d1);
-		System.out.println("포멧팅 된 날짜 : " + forDate);
+		System.out.println("포멧팅 된 년,월,일 : " + forDate);
 	
 	}	
 	
@@ -22,17 +22,17 @@ public class Practice4 {
 		Date d1 = new Date();
 		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
 		String forDate = sdf.format(d1);
-		System.out.println("포멧팅 된 시간 : " + forDate);
+		System.out.println("포멧팅 된 시,분,초 : " + forDate);
 	}
-`	
 	
 	public void pracitce3() {
 		Date d1 = new Date();
 		SimpleDateFormat sdf = new SimpleDateFormat("MM-dd");
 		String forDate = sdf.format(d1);
-		System.out.println("포멧팅 된 날짜 : " + forDate);
+		System.out.println("포멧팅 된 월,일 : " + forDate);
 			
 		}
+	
 	public void pracitce4() {
 		Date d1 = new Date();
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-HH");
@@ -41,6 +41,7 @@ public class Practice4 {
 			
 			
 	}
+	
 	public void pracitce5() {
 		Date d1 = new Date();
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd (E)");
