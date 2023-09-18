acba420602d543b618b499695b3b034f256fc80b
tjehdwo <143372343+tjehdwo@users.noreply.github.com>
2023-09-18 15:01:11 +0900

    generics

--- chap26_Generics/src/com/kh/genericspractice/PracticeMain.java
+++ chap26_Generics/src/com/kh/genericspractice/PracticeMain.java
@@ -50,7 +50,7 @@ public class PracticeMain {
 		printBox(intBox);
 		}
 		public static <T> void printBox(Box<T> intbox ) {
-			for (T item:intbox) {
+			for (T item:) {
 				System.out.println(item);
 				
 			}
