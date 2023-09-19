1c67d29925ea20e0d9e9e25df012ec72b96786a4
tjehdwo <143372343+tjehdwo@users.noreply.github.com>
2023-09-19 10:43:06 +0900

    interface

--- chap19_Interface/src/com/kh/foodStore/Order2.java
+++ chap19_Interface/src/com/kh/foodStore/Order2.java
@@ -3,18 +3,32 @@ package com.kh.foodStore;
 import java.util.ArrayList;
 import java.util.List;
 
+//주문 객체2 출력하는 문구 변경
 public class Order2 implements FoodStore{
-	private List<Menu> oder2menus;
+private List<Menu> ordermenus;
 	
 	public Order2() {
-		oder2menus = new ArrayList<>();
+		ordermenus = new ArrayList<>();//주문한 내용들
 	}
 	
 	@Override
 	public void addMenu(String menu) {
-		Menu menuB = new Menu(menu);
-		oder2menus.add
+		Menu menuA = new Menu(menu);
+		ordermenus.add(menuA);
 	}
 	
-
+	@Override
+	public void removeMenu(String menu) {
+		System.out.println(menu);
+	}
+	@Override
+	public void printMenu() {
+		
+		//menuA.getItem();
+	 System.out.println("dddd");
+		for (Menu menuA: ordermenus) {
+			System.out.println(menuA.getItem());
+		}
+		
+	}
 }
