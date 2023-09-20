2e847dd696c5834a695a900b0db2ac92ec4bb3b3
tjehdwo <143372343+tjehdwo@users.noreply.github.com>
2023-09-20 17:03:11 +0900

    thread

--- chap30_Thread/src/com/kh/sync/syncMethod.java
+++ chap30_Thread/src/com/kh/sync/syncMethod.java
@@ -24,10 +24,17 @@ public class syncMethod {
 	private int count = 0;
 	private Object lockObj = new Object();//동기화를 위한 객체
 	
+	//메서드 레벨 동기화를 사용해서 count 변수를 증가시킴
+	//한 번에 하나의 스레드만 접근할 수 있도록 보장
 	public synchronized void increment() {
 		count++;
 	}
-	
+	/*
+	 * pTask : 동기화된 블록과 함꼐 사용
+	 * synchronized(lockObj) : 코드 블록 내에서 count 변수를 증가시키기 전에  
+	 * 						   lockObj를 사용하여 동기화를 수행
+	 * lockObj : 다른 메서드에서 동기화에 사용할 수 있고, 다른 객체에 대한 동기화와 분리되어 충돌을 방지할 수 있음
+	 * */
 	public void pTask() {
 		// 다른 비동기 코드
 		synchronized(lockObj) {
@@ -37,10 +44,12 @@ public class syncMethod {
 		//다른 비동기 코드
 		
 	}
+	//syncMethod 클래스의 인스턴스를 생성
 	public static void main(String[] args) {
 		syncMethod sync = new syncMethod();
 		
-		// Runnable 인터페이스 구현을 사용해서 스레드 생성
+		// Runnable 인터페이스 구현을 사용해서 두 개의 스레드 생성
+		// thread1
 		Runnable task1 = new Runnable() {
 			@Override
 			public void run() {
@@ -49,7 +58,7 @@ public class syncMethod {
 				}
 			}
 		};
-		
+		// thread2
 		Runnable task2 = new Runnable() {
 			@Override
 			public void run() {
@@ -78,3 +87,9 @@ public class syncMethod {
 	}
 
 }
+
+/*synchronized count 변수에 대한 동시 접근을 동기화
+		스레드 간 경쟁 조건이 발생하지 않고
+		count 변수가 안전하게 증가
+		데이터의 일관성을 보존
+*/
