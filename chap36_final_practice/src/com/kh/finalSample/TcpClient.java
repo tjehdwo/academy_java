226204b2234e5fe160abab532d3fa6c797dd43a8
tjehdwo <143372343+tjehdwo@users.noreply.github.com>
2023-09-21 17:30:00 +0900

    finalP

--- chap36_final_practice/src/com/kh/finalSample/TcpClient.java
+++ chap36_final_practice/src/com/kh/finalSample/TcpClient.java
@@ -1,5 +1,9 @@
 package com.kh.finalSample;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.net.InetAddress;
 import java.net.Socket;
 
@@ -7,11 +11,23 @@ public class TcpClient {
 	
 	public static void main(String[] args) {
 		int port =3333;
-		String serverIP;
+		String serverIP; // 내 아이피 주소
 	try {
 		serverIP = InetAddress.getLocalHost().getHostAddress();
+		System.out.println("serverIP");
 		Socket socket = new Socket(serverIP, 3333);
-		System.out.println("서버 연결");
+		System.out.println("sever connect");
+		
+		//클라이언트와 통신을 위한 입 출력 스트림 설정
+		BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		OutputStream os = socket.getOutputStream();
+		
+		//클라이언트에서 서버로 메세지 전송
+		String msg = "hi . server";
+		os.write(msg.getBytes());
+		os.flush();
+		
+		socket.close();
 	}catch(Exception e) {
 		e.printStackTrace();
 	}
--- chap36_final_practice/src/com/kh/finalSample/TcpServer.java
+++ chap36_final_practice/src/com/kh/finalSample/TcpServer.java
@@ -1,7 +1,11 @@
 package com.kh.finalSample;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class TcpServer {
 	public static void main(String[] args) throws Exception{
@@ -10,11 +14,18 @@ public class TcpServer {
 	ServerSocket server;
 	
 	server = new ServerSocket(3333);
-	System.out.println(port);
-	System.out.println("서버 대기중");
+	System.out.println("서버가 포트" + port + "에서 실행 중입니다.");
 	while(true) {
 		Socket client = server.accept();
-		System.out.println(client.getInetAddress());
+		System.out.println("클라이언트 접속 : " + client.getInetAddress());
+		
+		InputStream is = client.getInputStream();
+		Scanner sc = new Scanner(is);
+	
+		OutputStream os = client.getOutputStream();
+		PrintStream ps = new PrintStream(os);
+		ps.print("hi client");
+		ps.flush();
 		System.out.println("클라이언트 연결");
 	}
 	
