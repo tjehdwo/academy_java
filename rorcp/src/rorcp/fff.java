943c7a148ddb0ca35661557a7308e78f0610a7d1
tjehdwo <143372343+tjehdwo@users.noreply.github.com>
2023-09-22 17:41:58 +0900

    rorcp

---
+++ rorcp/src/rorcp/fff.java
@@ -0,0 +1,18 @@
+package rorcp;
+
+public class fff {
+
+	public static void main(String[] args) {
+		int a = -4;
+		int b = 7;
+		boolean flag;
+		int answer = 0;
+	        if (flag = false){
+	            answer = a-b;
+	        }else{
+	            answer = a+b;
+	        }System.out.println("´ä : " + answer);
+	        
+	    }
+
+}
